6f632f3ff15564a2e721ae0b4be09c10c56c9b5d
Ivan Minyaev <i.onechesz@gmail.com>
2023-04-30 10:25:27 +0300

    The initial commit.

---
+++ src/main/java/com/github/onechesz/simplediet/SimpleDietApplication.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.simplediet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleDietApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleDietApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/onechesz/simplediet/SimpleDietApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.onechesz.simplediet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleDietApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
